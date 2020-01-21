f5783994699c246aaf00a5210488ddc29ff78619
ValdemirPraxedes <valdemirpraxedes92@gmail.com>
2020-01-21 10:33:43 -0300

    primeiro commit

---
+++ src/main/java/com/jun/crawler/novatec/NovatecApplication.java
@@ -0,0 +1,13 @@
+package com.jun.crawler.novatec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovatecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NovatecApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jun/crawler/novatec/NovatecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jun.crawler.novatec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NovatecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
